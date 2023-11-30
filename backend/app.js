3ab82c42bdff22af0695133b3a396d12eee647e1
usamahussaindev <usamahussain.dev@gmail.com>
2023-12-01 03:41:21 +0500

    Add middleware setup for cookie parsing, body parsing, JSON parsing, and file uploads using express

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+const ErrorHandler = require("./middleware/error");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({useTempFiles: true}));
+
 
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
