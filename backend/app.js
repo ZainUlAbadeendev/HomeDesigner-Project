3bbe200d98400aa13cb8b5390ad4253254bca2fd
usamahussaindev <usamahussain.dev@gmail.com>
2023-12-01 05:42:47 +0500

    Updated relevant routes and controllers to process file uploads

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
+
 
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({useTempFiles: true}));
+
 
 
 //config
@@ -18,6 +18,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
       path: "config/.env",
     });
 }
+
+
+// import routes
+const user = require("./controller/user");
+
+app.use("/api/v2/user", user);
+
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
